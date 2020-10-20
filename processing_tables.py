674f5d56666e0fb9dc35876ff13516fbcd48a7ea
basileia <jane.novak@centrum.cz>
2020-10-20 17:55:06 +0200

    function for all football matches info sorted by date

--- processing_tables.py
+++ processing_tables.py
@@ -4,8 +4,7 @@ import datetime
 
 def load_json(file_name):
     """
-    Loads given json file and return pair of two teams. The first one is
-    home team and the second is away team.
+    Loads given json file
     """
     with open(file_name, encoding="utf-8") as json_file:
         data = json.load(json_file)
@@ -14,7 +13,9 @@ def load_json(file_name):
 
 def get_info(number):
     """
-    Get information about a football match by table number, returns list
+    Get information about a football match by table number, returns list with:
+    date, home team, away team and score
+    Date is reformatting
     """
     wanted_team = "FC Sigma Hodolany (2)"
     results = load_json("table" + str(number) + ".json")
@@ -27,6 +28,7 @@ def get_info(number):
         if wanted_team in results["domácí"][i]:
             info = [results["datum a čas"][i], wanted_team,
                     results["hosté"][i], results["skóre"][i]]
+    info[0] = time_formatting(info[0])
     return info
 
 
@@ -37,5 +39,20 @@ def time_formatting(date_time):
     return datetime.datetime.strptime(date_time, '%d.%m.%Y %H:%M')
 
 
-list_result = get_info(1)
-print(time_formatting(list_result[0]))
+def sort_by_date(list_info):
+    return sorted(list_info, key=lambda x: x[0])
+
+
+def get_matches_info():
+    """
+    Get info about football matches in the competition
+    Returns lists of matches sorted by date
+    """
+    matches_info = []
+    for i in range(1, 30):
+        matches_info.append(get_info(i))
+    sorted_matches_info_date = sort_by_date(matches_info)
+    return sorted_matches_info_date
+
+
+print(get_matches_info())
