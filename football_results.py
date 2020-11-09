8277e94d362970f26693322f7e4631cb4de350b7
basileia <jane.novak@centrum.cz>
2020-11-09 15:29:55 +0100

    fix time.sleep() to WebDriverWait with expected_conditions

--- football_results.py
+++ football_results.py
@@ -1,43 +1,40 @@
 from selenium import webdriver
 from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
-import time
 import json
 
 
-def download_results():
+def download_results(competition_number):
     """
-    Downloads Hodolany match results from is.fotbal.cz
+    Downloads FC Hodolany match results from is.fotbal.cz found by competition
+    number
     """
     options = Options()
     options.headless = True
     driver = webdriver.Chrome('C:\\webdrivers\\chromedriver', options=options)
     driver.get("https://is.fotbal.cz/")
-
     competitions_xpath = '//*[@id="TopMenu_liSoutez"]/a'
     competition_number_field_xpath = '//*[@id="txtSearchCislo"]'
     search_button_xpath = '//*[@id="btnSearch"]/span'
     competition_name_xpath = '//*[@id="MainContent_gridData"]/tbody/tr[2]/td[2]/a'
-
-    competition_number = "2020712A1A"
-    driver.find_element_by_xpath(competitions_xpath).click()
-    time.sleep(1)
-    driver.find_element_by_xpath(competition_number_field_xpath).send_keys(competition_number)
-    time.sleep(2)
-    driver.find_element_by_xpath(search_button_xpath).click()
-    time.sleep(1)
-
+    elements = [competitions_xpath, competition_number_field_xpath,
+                search_button_xpath]
+    wait = WebDriverWait(driver, 10)
+    for elem in elements:
+        wait.until(EC.element_to_be_clickable((By.XPATH, elem))).click()
+        if elem == competition_number_field_xpath:
+            driver.find_element_by_xpath(competition_number_field_xpath).send_keys(competition_number)
     while True:
         try:
-            driver.find_element_by_xpath(competition_name_xpath).click()
+            wait.until(EC.element_to_be_clickable((By.XPATH, competition_name_xpath))).click()
             break
         except ElementClickInterceptedException:
-            time.sleep(2)
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-    url = driver.current_url
-    tables = pd.read_html(url)
+    tables = pd.read_html(driver.current_url)
     driver.close()
     info_to_json = []
     for table in tables:
@@ -55,4 +52,5 @@ def write_json(data, filename='tables.json'):
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 
-write_json(download_results())
+competition_number = "2020712A1A"
+write_json(download_results(competition_number))
